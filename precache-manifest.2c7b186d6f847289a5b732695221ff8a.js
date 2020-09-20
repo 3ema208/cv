fff5b718e7a0f09c8181072f20e8d5fe08376c08
3ema208 <roman94eng@gmail.com>
2020-09-21 01:20:57 +0300

    Updates

---
+++ precache-manifest.2c7b186d6f847289a5b732695221ff8a.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8bea4fc04c62bea14f40abb96f62b83a",
+    "url": "/cv/index.html"
+  },
+  {
+    "revision": "2beac4cc656943d152f0",
+    "url": "/cv/static/css/main.7fe9eaa8.chunk.css"
+  },
+  {
+    "revision": "f49623823d6987419b8c",
+    "url": "/cv/static/js/2.87c1a377.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/cv/static/js/2.87c1a377.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2beac4cc656943d152f0",
+    "url": "/cv/static/js/main.7b362456.chunk.js"
+  },
+  {
+    "revision": "3724c5707af353aab8a0",
+    "url": "/cv/static/js/runtime-main.76204999.js"
+  },
+  {
+    "revision": "806fc86d0a214fb4fa472dbc236011c7",
+    "url": "/cv/static/media/Marlboro.806fc86d.ttf"
+  },
+  {
+    "revision": "9dc5a0181ce6f0344e1bc958c78cd13f",
+    "url": "/cv/static/media/avatar.9dc5a018.jpg"
+  }
+]);
